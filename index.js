106276a07a7a98ab07249125dca1b2ad548256ee
Pevio <pwgossell@gmail.com>
2023-05-13 19:33:13 -0500

    dgcr member analysis

---
+++ index.js
@@ -0,0 +1,73 @@
+import axios from "axios"
+import dayjs from "dayjs"
+
+// Top Header Row
+const thisYear = dayjs().get("year")
+let topRow = "Username,Profile ID,DGCR Member Page,"
+for (let i = 2007; i <= thisYear; i++) {
+  topRow += `${i},`
+}
+topRow += "Total Reviews"
+console.log(topRow)
+
+const dateStart = "<td align=\"center\">\n"
+for (let i = 1; i <= 200; i++) {
+  // Get the user page
+  const url = `https://www.dgcoursereview.com/profile.php?id=${i}`
+  const response = await axios.get(url)
+  const data = response.data;
+
+  // username, and make sure user is real
+  const usernameStart = data.indexOf("<title>")
+  if (data.substring(usernameStart + 7, usernameStart + 30) === "Disc Golf Course Review") continue;
+  let username = ""
+  while (true) {
+    const nextChar = data[usernameStart + 7 + username.length]
+    if (nextChar === "'") break;
+    username += nextChar
+  }
+
+  // Make sure the user has at least 1 review
+  const totalReviewStart = data.indexOf("<strong>Reviews:</strong>")
+  let totalReviewText = ""
+  while (true) {
+    const nextChar = data[totalReviewStart + 26 + totalReviewText.length]
+    if (nextChar === "'") break;
+    totalReviewText += nextChar
+  }
+  const totalReviews = Number.parseInt(totalReviewText)
+  if (totalReviews === 0) continue;
+  
+  const yearCounts = Array(17).fill(0) // number of reviews per year
+
+  // Review indexes
+  const reviewStart = data.indexOf("Review Date")
+  const reviewEnd = data.indexOf("<td>Favorite</td>")
+  var regex = /<td align="center">/gi, result, indices = [];
+  while ( (result = regex.exec(data)) ) {
+      indices.push(result.index);
+  }
+  // Review dates
+  indices.forEach((strIndex, arrIndex) => {
+    if (strIndex > reviewStart && strIndex < reviewEnd && arrIndex % 3 === 0) {
+      let text = "";
+      let currentIndex = strIndex + dateStart.length
+      while (true) {
+        if (data[currentIndex] === " ") {
+          yearCounts[dayjs(text).year() - 2007] += 1;
+          break;
+        }
+        text += data[currentIndex]
+        currentIndex += 1;
+      }
+    }
+  })
+
+  // Compile results
+  let resultText = `${username},${i},${url},`
+  yearCounts.forEach((count) => {
+    resultText += `${count},`
+  })
+  resultText += String(totalReviews)
+  console.log(resultText)
+}
